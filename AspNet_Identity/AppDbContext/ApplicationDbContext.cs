f03579468d6d1a2c1c56ae5ad63a2e11e9a55f49
Asif Rahman <asif0531@live.com>
2018-09-26 18:44:42 +0600

    all required package installed

--- AspNet_Identity/AppDbContext/ApplicationDbContext.cs
+++ AspNet_Identity/AppDbContext/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using AspNet_Identity.DataModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AspNet_Identity.AppDbContext
 {
-    class ApplicationDbContext
+    class ApplicationDbContext : IdentityDbContext<UserModel, RoleModel, string>
     {
     }
 }
---
+++ AspNet_Identity/DataModel/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNet_Identity.DataModel
+{
+    public class UserModel : IdentityUser
+    {
+
+    }
+}
